2ec8912f9d4302e61fdeaa335b25abdc963cefbd
Muhammet Sanci <9muhammetsanci10@gmail.com>
2025-01-02 19:57:39 +0300

    bozdum

--- src/App.js
+++ src/App.js
@@ -1,75 +1,20 @@
 import React from "react";
-import BlogCard from "./components/BlogCard";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 import "./App.css";
-import { Helmet } from "react-helmet";
 
 function App() {
-  const blogPosts = [
-    {
-      id: 1,
-      title: "Hikaye 1: Bir Başlangıç",
-      description: "Bu, harika bir başlangıcın hikayesi...",
-      date: "2025-01-02",
-    },
-    {
-      id: 2,
-      title: "Hikaye 2: Yolculuk Devam Ediyor",
-      description: "Hayat, bir yolculuktur ve bu hikaye tam ortasında.",
-      date: "2025-01-01",
-    },
-    {
-      id: 3,
-      title: "Hikaye 3: Hedefe Yaklaşırken",
-      description: "Hedefe az kaldı ama hikaye bitmedi...",
-      date: "2024-12-31",
-    },
-  ];
-
   return (
-    
-      
-    <div className="App">
-      <header className="header">
-        <h1>TEKSANBİL</h1>
-        <p>Teknoloji sanat ve bilim hikayeleri</p>
-      </header>
-      <Helmet>
-      <title>Blog - Teksanbil</title>
-      </Helmet>
-    
-      <main className="main">
-        {blogPosts.map((post) => (
-          <BlogCard key={post.id} post={post} />
-        ))}
-      </main>
+    <div>
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App*/
+/* */ 
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Barış Özcan Tarzı Web Sitesi</h1>
+      <p>Minimalist, Modern ve Profesyonel</p>
+    </div>
+  );
+}
+
+export default Hero;
